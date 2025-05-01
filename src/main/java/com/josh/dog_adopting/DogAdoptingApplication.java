77d03a4bee7fb76e8739deba23b5d70e8a0a9076
Griotold <gotjd9773@naver.com>
2025-05-01 13:14:03 +0900

    Initial commit

---
+++ src/main/java/com/josh/dog_adopting/DogAdoptingApplication.java
@@ -0,0 +1,13 @@
+package com.josh.dog_adopting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAdoptingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogAdoptingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josh/dog_adopting/DogAdoptingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.dog_adopting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAdoptingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
